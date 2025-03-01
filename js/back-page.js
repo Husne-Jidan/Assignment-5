4248d11e07a0f4281c3c48258023dc37e72120b7
HUSNEJIDAN <119681826+HUSNEJIDAN@users.noreply.github.com>
2025-03-01 10:20:04 +0600

    complete btn work remain

---
+++ js/back-page.js
@@ -0,0 +1,6 @@
+document.getElementById("back-page").addEventListener('click', function( event){
+    event.preventDefault();
+    //console.log('Button Clicked');
+    window.location.href = 'index.html'
+    
+});
---
+++ js/index.js
@@ -0,0 +1,5 @@
+document.getElementById("next-page").addEventListener('click', function( event){
+    event.preventDefault();
+    //console.log('Button Clicked');
+    window.location.href = 'next.html'
+});
